06b2f01610d5f1cd95f10020cdf8c7ca402e7ac7
John Nate <me@jonathanhosea.dev>
2023-02-14 09:51:02 +0800

    WIP

--- src/main/scala/dev/lucasgrey/flow/indexer/daemon/BlockMonitor.scala
+++ src/main/scala/dev/lucasgrey/flow/indexer/daemon/BlockMonitor.scala
@@ -1,13 +1,51 @@
 package dev.lucasgrey.flow.indexer.daemon
 
-import com.nftco.flow.sdk.{FlowAccessApi,Flow}
+import akka.{Done, NotUsed}
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.stream.scaladsl.{Sink, Source}
+import com.nftco.flow.sdk.{Flow, FlowAccessApi, FlowBlockHeader}
+import dev.lucasgrey.flow.indexer.config.ConfigHolder
 
-object BlockMonitor {
-  val accessAPI: FlowAccessApi = Flow.newAccessApi("", 0)
+import scala.concurrent.{ExecutionContext, Future}
 
-  def getLatestBlockId() = {
-    this.accessAPI.getLatestBlockHeader().getId()
+class BlockMonitor {
+
+  import BlockMonitor._
+
+  implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "actor")
+  implicit val executionContext: ExecutionContext = actorSystem.executionContext
+
+  def StartPolling(): Future[Done] = {
+    Source.unfold(startHeight) { currentHeight =>
+      if (currentHeight == getLatestHeight) {
+        Thread.sleep(100)
+        None
+      } else {
+        Some(currentHeight + 1, currentHeight)
+      }
+    }
+      .mapAsync(1) { height =>
+        for {
+          _ <- Future.unit
+          blockHeader = getBlockHeader(height)
+        } yield ()
+      }
+      .runWith(Sink.ignore)
   }
+
+  private def getLatestHeight: Long = {
+    accessAPI.getLatestBlockHeader.getHeight
+  }
+
+  private def getBlockHeader(blockHeight: Long): Future[FlowBlockHeader] = {
+    Future.successful(accessAPI.getBlockHeaderByHeight(blockHeight))
+  }
+}
+
+object BlockMonitor extends ConfigHolder {
+  val startHeight: Long = config.getString("start-height").toLong
+  val accessAPI: FlowAccessApi = Flow.newAccessApi("", 0)
 
 }
 
